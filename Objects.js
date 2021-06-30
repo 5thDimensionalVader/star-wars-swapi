2895e43b14e10c042f9fc43c03965827dbac8399
jdotcole233 <jdotcole233@gmail.com>
2021-06-30 11:25:06 +0000

    some object information

--- Objects.js
+++ Objects.js
@@ -1,16 +1,22 @@
 const human = {
     name: "cole",
     age: 1000,
-    heigh: 9,
-    complexion: "caramel",
-    setName: function (name) {
-        this.name = name;
-    }
+    height: 9,
+    complexion: "caramel"
 }
 
-const keys = Object.keys(human);
-console.log(keys);
-Object.values(human).forEach(val => console.log(val));
+
+console.log(Object.entries(human));
+
+// console.log(human.name);
+// human.setName("Daniel");
+// console.log(human.name);
+
+
+
+// const keys = Object.keys(human);
+// console.log(keys);
+// Object.values(human).forEach(val => console.log(val));
 
 
 // for (const [key, value] of  Object.entries(human)) {
